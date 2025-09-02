d8aa45e607fb6044a58c39c6aa0a8acb2b1ef36c
Alokswain <alokdhana123@gmail.com>
2025-09-02 09:33:48 +0530

    Added simple login test

---
+++ automation-tests/README.md
@@ -0,0 +1,13 @@
+# Selenium Automation Framework
+
+This is a TestNG-based Selenium framework using WebDriverManager for automatic driver management.
+
+## Features
+- Page Object Model
+- TestNG integration
+- Maven build
+- WebDriverManager support
+
+## How to Run
+```bash
+mvn test
---
+++ automation-tests/src/test/java/com/alok/selenium/tests/LoginTest.java
@@ -0,0 +1,36 @@
+package com.alok.selenium.tests;
+
+import com.alok.selenium.base.BaseTest;
+import com.alok.selenium.pages.LoginPage;
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LoginTest extends BaseTest { 
+
+    @Test
+    public void validLogin_shouldNavigateToSecureArea_andShowSuccess() {
+        LoginPage login = new LoginPage(driver).open();
+        login.loginAs("tomsmith", "SuperSecretPassword!");
+
+        assertThat(login.atSecureArea())
+                .as("User should land on secure area after valid login")
+                .isTrue();
+
+        assertThat(login.getFlashMessage())
+                .contains("You logged into a secure area!");
+    }
+
+    @Test
+    public void invalidLogin_shouldShowError() {
+        LoginPage login = new LoginPage(driver).open();
+        login.loginAs("wronguser", "wrongpass");
+
+        assertThat(login.atSecureArea())
+                .as("User should NOT land on secure area for invalid login")
+                .isFalse();
+
+        assertThat(login.getFlashMessage())
+                .contains("Your username is invalid"); // partial match is enough
+    }
+}
